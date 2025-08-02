d784a2087f7e9d0e234ec6776467cd8839e09b37
ngp263 <154538282+ngp263@users.noreply.github.com>
2025-08-03 02:14:04 +0800

    Update itunes.js

--- itunes.js
+++ itunes.js
@@ -10,7 +10,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/ngp263/woyaofendou/refs/heads/main/itunes.js
 
 [mitm]
 hostname = buy.itunes.apple.com
