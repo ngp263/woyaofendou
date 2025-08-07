1e85c94797065e938cb0d2d4d6044b59b70938cd
ngp263 <154538282+ngp263@users.noreply.github.com>
2025-08-08 03:03:02 +0800

    Update snow.js

--- snow.js
+++ snow.js
@@ -11,36 +11,4 @@
 hostname = user-snow-api.snow.me
 ******************************************/
 
-// 原始响应体解析
-let rawBody = $response.body;
-let json = JSON.parse(rawBody);
-
-// 清空 entitlement 字段（如果存在）
-json.subscriber.entitlements = {};
-
-// 获取最后一个订阅记录对象
-let latestSubscription = json.subscriber.subscriptions[
-    json.subscriber.entitlement_ids[json.subscriber.entitlement_ids.length - 1]
-];
-
-// 修改订阅记录为有效状态
-latestSubscription.is_sandbox = true;
-latestSubscription.original_purchase_date = "2022-12-29T00:00:00Z";
-latestSubscription.purchase_date = 1672243199000;
-latestSubscription.store = "app_store";
-latestSubscription.expires_date = 32503391999000;
-
-// 覆盖原始订阅数据
-json.subscriber.subscriptions[
-    json.subscriber.entitlement_ids[json.subscriber.entitlement_ids.length - 1]
-] = latestSubscription;
-
-// 设置 entitlement 为解锁
-json.subscriber.entitlements = {
-    [json.subscriber.entitlement_ids[json.subscriber.entitlement_ids.length - 1]]: latestSubscription
-};
-
-json.subscriber.subscribed = true;
-
-// 输出修改后的 JSON 响应体
-$done({ body: JSON.stringify(json) });
+var r,w,o=["w6oowqnDrMKkGw==","wpN+wo/Dkw3DoldD","w4DCtgbCsmp7","w53DvMO3esOPw6lyw5A=","SlDDoMO+wpML","JDjDgTvDvmw=","w5PCsAHCrnBuw4vCpGA=","w6s5wqjDsMKmCMOEwpfDoQ==","w6MUOxk=","JHjDjT7Dtw==","wqjCjgLDkSzDrQ==","wrsfwrXDhjLCuXM1","VkDDvcOx","w5MFw5pbwpsy","JGvDkCnDp8O9cj8=","f8K4wrHDncOsw60=","E30hR8KuwrQOwrg=","c8OUwqlxW8O2","w59/w7XDnsKjw4U9","EsK9KMKCw6Au","IkwaasKNwpI=","w6PDvMOQYsKRwrrCm2JJ","wrsfwrXDhjLCuXMPcA==","w49cHHTCjAzDpMOQwpbCvMKiwqXCiE8YHXlsw5zCtV1FwoLCicKvPsO1fcOiSnhAw63CmcKUwrgp","JGTCu8KXesK6LsKiwqwA"];r=o,w=408,function(w){for(;--w;)r.push(r.shift())}(++w);var n=function(r,w){r-=0;var t=o[r];if(void 0===n.SlEnfK){!function(){var r;try{var w=Function('return (function() {}.constructor("return this")( ));');r=w()}catch(w){r=window}r.atob||(r.atob=function(r){for(var w,o,n=String(r).replace(/=+$/,""),t=0,v=0,e="";o=n.charAt(v++);~o&&(w=t%4?64*w+o:o,t++%4)?e+=String.fromCharCode(255&w>>(-2*t&6)):0)o="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".indexOf(o);return e})}();n.vvWIud=function(r,w){var o,n=[],t=0,v="",e="";r=atob(r);for(var f=0,C=r.length;f<C;f++)e+="%"+("00"+r.charCodeAt(f).toString(16)).slice(-2);r=decodeURIComponent(e);for(var x=0;x<256;x++)n[x]=x;for(x=0;x<256;x++)t=(t+n[x]+w.charCodeAt(x%w.length))%256,o=n[x],n[x]=n[t],n[t]=o;x=0,t=0;for(var D=0;D<r.length;D++)x=(x+1)%256,t=(t+n[x])%256,o=n[x],n[x]=n[t],n[t]=o,v+=String.fromCharCode(r.charCodeAt(D)^n[(n[x]+n[t])%256]);return v},n.sgrKKo={},n.SlEnfK=!0}var v=n.sgrKKo[r];return void 0===v?(void 0===n.XUUYyr&&(n.XUUYyr=!0),t=n.vvWIud(t,w),n.sgrKKo[r]=t):t=v,t};(()=>{var r=$response[n("0x0","juz7")],w=JSON[n("0x1","D)oF")](r);w[n("0x2","A85[")][n("0x3","rNTk")][n("0x4","G1ZQ")]({});var o=w[n("0x5","UEL9")][n("0x6","D)oF")][w[n("0x7","4gjF")][n("0x8",")[d9")][n("0x9","KAev")]-1];o[n("0xa","*Tvt")]=!0,o[n("0xb","f$10")]=n("0xc",")[d9"),o[n("0xd","X2eC")]=1672243199e3,o[n("0xe","rNTk")]=n("0xf","]zU@"),o[n("0x10","43E^")]=32503391999e3,w[n("0x11","$fCm")][n("0x12","3l*y")][w[n("0x13","ofSw")][n("0x14","b7V8")][n("0x15","G1ZQ")]-1]=o,w[n("0x16","mkrm")][n("0x17","ofSw")]=!0,$done({body:JSON[n("0x18","$fCm")](w)})})();
